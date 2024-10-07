144f20751905acefbb4e405f018fefda9db4b797
Nguyenhieu277 <nguyenphamtrunghieu277@gmail.com>
2024-10-07 15:55:06 +0700

    Generate

---
+++ Python4Latex_TRR1/generateBeforeCombination.py
@@ -0,0 +1,19 @@
+import random
+from thuvien import *
+NumberQuestions = 2
+BankQuestions = []
+
+def nextCombination(combination):
+    int index = 5
+    while index >= 0 and combination[index] == i + 3:
+        
+
+for number in range(NumberQuestions):
+    combination = []
+    a, b, c, d, e = random.sample(range(1, 10), 5)
+    combination.append((a, b, c, d, e))
+    ProblemStatement = "Cho tập $A = {1, 2, 3, 4, 5, 6, 7, 8, 9}$. Giả sử áp dụng phương pháp sinh tổ hợp chập $k$ của một " + \
+                       "tập hợp theo thứ tự từ điển, hãy liệt kê 5 tổ hợp chập 5 liền kề tiếp theo của tổ hợp $(" + str(a) + ", " + \
+                       str(b) + ", " + str(c) + ", " + str(d) + ", " + str(e) + ").$"
+    
+    
